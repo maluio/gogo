526882932a3a42a54c5d484cccf7a5f3571a9462
Malu <malu@example.com>
2018-02-24 18:15:21 +0100

    Add simplemde markdown editor

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,17 @@
 var $ = require('jquery');
 var learn = require('./learn');
 
+var simpleMDE = require('simplemde/dist/simplemde.min.js');
+
 $(function () {
     learn();
+
+    $('textarea').each(function () {
+        new simpleMDE({
+            element: $(this)[0],
+            forceSync: true
+        });
+    });
 
     $('.alert').fadeIn(200).delay(1000).fadeOut(200);
 });
