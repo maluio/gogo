537eaf576fc975d92b93eb75e77364b499940eff
Malu <malu@example.com>
2018-03-01 13:38:50 +0100

    Add react learn basic working example

--- assets/js/learn.jsx
+++ assets/js/learn.jsx
@@ -1,17 +1,87 @@
 let React = require('react');
 let ReactDOM = require('react-dom');
 
-class Learn extends React.Component {
-    constructor(props) {
-        super(props);
-        console.log(props);
-    }
+class Rate extends React.Component{
+    rate(rating){
+        fetch('/react/learn/rate/' + this.props.itemId, {
+            method: 'POST',
+            body: JSON.stringify({
+                learn_rating: rating,
+            }),
+            headers: {
+                'Authorization': this.props.pw
+            },
+        }).then(res => {
+            location.reload();
+        })
+            .then(
+                (result) => {
+       //
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+    //
+                }
+            )
+    };
 
     render(){
-        return (<h1>hi</h1>);
+        let buttons = [];
+
+        for (let i = 0; i < 6; i++) {
+            buttons.push(
+                <button onClick={() => this.rate(i)} key={i} type="submit" className="btn btn-light btn-lg" name="learn_rating" value={i}>{i}</button>
+            )
+        }
+
+        return (
+            <div>{buttons}</div>
+        )
     }
 }
 
-ReactDOM.render(<Learn />, document.getElementById("learn-jsx"));
+class Card extends React.Component{
+    render(){
+        return <div className="row">
+            <div className="card card-outline-secondary mb-3">
+                <div className="card-body">
+                    <p>{this.props.content}</p>
+                </div>
+            </div>
+        </div>;
+    }
+}
 
-console.log(GoGo.item.question);
+class Learn extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            showResults: false
+        };
+    }
+
+    toggleShowAnswer() {
+        this.setState((prevState, props) => {
+            return { showResults: !prevState.showResults }
+        });
+    };
+
+    render() {
+
+        return (
+            <div id="learn-view">
+                <Card content={this.props.item.question}/>
+                <Rate itemId={this.props.item.id} pw={this.props.pw}/>
+                <button onClick={() => this.toggleShowAnswer()} className="btn btn-light js-show btn-lg btn-block">
+                    <span className="oi oi-elevator"></span>
+                </button>
+                { this.state.showResults ? <Card content={this.props.item.answer}/> : null }
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Learn item={GoGo.item}  pw={GoGo.basicAuthValue}/>, document.getElementById("learn-jsx"));
--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -3,18 +3,55 @@
 
 namespace App\Controller;
 
+use App\AppConstants;
+use App\Entity\Item;
+use App\Entity\Rating;
+use App\Learn\LearnHandlerInterface;
 use App\Repository\ItemRepository;
+use App\Utils\DateTimeFormatHelper;
+use Doctrine\ORM\EntityManagerInterface;
 use JMS\Serializer\Serializer;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class TestController extends Controller
 {
+    /**
+     * @Route("/react/learn/rate/{item}", name="learn_rate")
+/*     */
+    public function rate(
+        Item $item,
+        EntityManagerInterface $em,
+        Request $request,
+        DateTimeFormatHelper $dateTimeFormatHelper,
+        LearnHandlerInterface $learnHandler
+    ){
+        $content = $request->getContent();
+        $content = json_decode($content, true);
+
+        $rating = $content['learn_rating'];
+
+        $newDueDate = $learnHandler->handle($item, $rating)->getNewDueDate();
+
+        $item->setDueAt($newDueDate);
+        $item->addRating(new Rating($rating));
+
+        $em->persist($item);
+        $em->flush();
+
+        $this->addFlash(
+            AppConstants::FLASH_DEFAULT,
+            'Due in ' . $dateTimeFormatHelper->formatDueDiff($newDueDate)
+        );
+
+        return $this->redirectToRoute('learn');
+    }
 
     /**
      * @Route("/react/", name="learn_react")
      */
-    public function react(ItemRepository $itemRepository)
+    public function react(ItemRepository $itemRepository, Request $request)
     {
         $dueItem = $itemRepository->findLatestDue();
 
@@ -22,8 +59,10 @@ class TestController extends Controller
 
         return $this->render('learn/react.html.twig',
             [
-                'item' => $dueItem
+                'item' => $dueItem,
+                'basicAuthValue' => $request->headers->get('Authorization')
             ]
         );
     }
+
 }
